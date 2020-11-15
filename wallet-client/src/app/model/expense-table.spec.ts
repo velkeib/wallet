7ff4a408c7892a4b2e5f649e3deaf4ab34b2a7e3
Velkei Bence <velkeib@gmail.com>
2020-11-15 15:19:47 +0100

    remove client to move to separate repo

--- wallet-client/src/app/model/expense-table.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ExpenseTable } from './expense-table';
-
-describe('ExpenseTable', () => {
-  it('should create an instance', () => {
-    expect(new ExpenseTable()).toBeTruthy();
-  });
-});
